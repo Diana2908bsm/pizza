351d7edba5ace5fc13cbcfff4dc0a6bd3aaa27d7
diana <diana.barantseva@gg.agency>
2023-01-31 15:41:48 +0300

    js-1

--- js/script.js
+++ js/script.js
@@ -71,3 +71,53 @@ window.addEventListener('click', function (event) {
     }
 
 })
+// -------------------------добавление карточки в корзину------------------------------------------
+const popup = document.querySelector('.popup')
+window.addEventListener('click', function (event) {
+
+    if (event.target.hasAttribute('data-cart')) {
+        const card = event.target.closest('.menu__item')
+        const productInfo = {
+            id: card.dataset.id,
+            imgSrc: card.querySelector('.menu__img').getAttribute('src'),
+            title: card.querySelector('.menu-item__title').innerText,
+            price: card.querySelector('.menu__price').innerText,
+            counter: card.querySelector('[data-counter]').innerText,
+
+        }
+        const itemInCart = popup.querySelector(`[data-id="${productInfo.id}"]`);
+
+        console.log(itemInCart)
+        if (itemInCart) {
+            const counterElement = itemInCart.querySelector('.popup__counter');
+            console.log(counterElement)
+            counterElement.innerText = parseInt(counterElement.innerText) + parseInt(productInfo.counter);
+            console.log(counterElement)
+        }
+        else {
+
+
+            const cartItemHTML = `  
+            <div class="popup__container"  data-id="${productInfo.id}">
+            <div class="popup__img">
+                <img src="${productInfo.imgSrc}" alt="">
+            </div>
+            <div class="popup__title">${productInfo.title}</div>
+            <div class="popup__info">
+                <div class="popup__price">Цена : ${productInfo.price}</div>
+                <div class="popup__box">
+                <div class="popup__title-counter">Кол-во :</div>
+                <div class="popup__counter"> ${productInfo.counter}</div>
+                </div>
+            </div>
+           </div>`;
+
+
+
+            popup.insertAdjacentHTML('beforeend', cartItemHTML)
+        }
+
+    }
+
+})
+
