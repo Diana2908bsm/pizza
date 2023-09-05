2f4e0237a390bf4b41b7250c4946baa994cae207
diana <diana.barantseva@gg.agency>
2023-09-05 14:45:50 +0300

    новый сайт

--- js/script.js
+++ js/script.js
@@ -1,157 +1,160 @@
-const menu = document.querySelector('.menu__inner'),
-    items = document.querySelectorAll('.menu__item'),
-    menuName = document.querySelectorAll('.menu__name')
+// const menu = document.querySelector('.menu__inner'),
+//     items = document.querySelectorAll('.menu__item'),
+//     menuName = document.querySelectorAll('.menu__name')
 
-function filter() {
-    menu.addEventListener('click', e => {
-        const targetId = e.target.dataset.id
-        const target = e.target
-        if (target.classList.contains('menu__name')) {
-            menuName.forEach(item => {
-                item.classList.remove('menu-active')
-            })
-            target.classList.add('menu-active')
-        }
-        switch (targetId) {
-            case "all":
-                getItems('menu__item')
-                break
-            case "meat":
-                getItems(targetId)
-                break
-            case "vegetables":
-                getItems(targetId)
-                break
-            case "seafood":
-                getItems(targetId)
-                break
-            case "mushroom":
-                getItems(targetId)
-                break
-        }
-    })
-}
-filter()
-function getItems(className) {
-    items.forEach(item => {
-        if (item.classList.contains(className)) {
-            item.style.display = 'block'
-        }
-        else {
-            item.style.display = 'none'
-        }
-    })
-}
-// ------------------burger------------------------------------------------
-const burger = document.querySelector('.header__burger');
-const list = document.querySelector('.header-top__column')
-const body = document.querySelector('body')
-burger.addEventListener('click', () => {
-    burger.classList.toggle('active')
-    list.classList.toggle('active')
-    body.classList.toggle('hide')
-})
-// ---------------------счетчик---------------------------------
-window.addEventListener('click', function (event) {
-    let counter;
+// function filter() {
+//     menu.addEventListener('click', e => {
+//         const targetId = e.target.dataset.id
+//         const target = e.target
+//         if (target.classList.contains('menu__name')) {
+//             menuName.forEach(item => {
+//                 item.classList.remove('menu-active')
+//             })
+//             target.classList.add('menu-active')
+//         }
+//         switch (targetId) {
+//             case "all":
+//                 getItems('menu__item')
+//                 break
+//             case "meat":
+//                 getItems(targetId)
+//                 break
+//             case "vegetables":
+//                 getItems(targetId)
+//                 break
+//             case "seafood":
+//                 getItems(targetId)
+//                 break
+//             case "mushroom":
+//                 getItems(targetId)
+//                 break
+//         }
+//     })
+// }
+// filter()
+// function getItems(className) {
+//     items.forEach(item => {
+//         if (item.classList.contains(className)) {
+//             item.style.display = 'block'
+//         }
+//         else {
+//             item.style.display = 'none'
+//         }
+//     })
+// }
+// // ------------------burger------------------------------------------------
+// const burger = document.querySelector('.header__burger');
+// const list = document.querySelector('.header-top__column')
+// const body = document.querySelector('body')
+// burger.addEventListener('click', () => {
+//     burger.classList.toggle('active')
+//     list.classList.toggle('active')
+//     body.classList.toggle('hide')
+// })
+// // ---------------------счетчик---------------------------------
+// window.addEventListener('click', function (event) {
+//     let counter;
 
-    if (event.target.dataset.action === 'plus' || event.target.dataset.action === 'minus') {
-        const counterWrapper = event.target.closest('.menu__dobov')
-        counter = counterWrapper.querySelector('[data-counter]')
-
-
-    }
-    if (event.target.dataset.action === 'plus') {
-        counter.innerText = ++counter.innerText
-    }
-    if (event.target.dataset.action === 'minus') {
-        if (parseInt(counter.innerText) > 1) {
-            counter.innerText = --counter.innerText;
-        }
-    }
-
-})
-// -------------------------добавление карточки в корзину------------------------------------------
-const popup = document.querySelector('.popup')
-window.addEventListener('click', function (event) {
-
-    if (event.target.hasAttribute('data-cart')) {
-        const card = event.target.closest('.menu__item')
-
-        const productInfo = {
-            id: card.dataset.id,
-            imgSrc: card.querySelector('.menu__img').getAttribute('src'),
-            title: card.querySelector('.menu-item__title').innerText,
-            price: card.querySelector('.menu__price').innerText,
-            counter: card.querySelector('[data-counter]').innerText,
-
-        }
-        const itemInCart = popup.querySelector(`[data-id="${productInfo.id}"]`);
+//     if (event.target.dataset.action === 'plus' || event.target.dataset.action === 'minus') {
+//         const counterWrapper = event.target.closest('.menu__dobov')
+//         counter = counterWrapper.querySelector('[data-counter]')
 
 
-        if (itemInCart) {
-            const counterElement = itemInCart.querySelector('.popup__counter');
+//     }
+//     if (event.target.dataset.action === 'plus') {
+//         counter.innerText = ++counter.innerText
+//     }
+//     if (event.target.dataset.action === 'minus') {
+//         if (parseInt(counter.innerText) > 1) {
+//             counter.innerText = --counter.innerText;
+//         }
+//     }
 
-            counterElement.innerText = parseInt(counterElement.innerText) + parseInt(productInfo.counter);
+// })
+// // -------------------------добавление карточки в корзину------------------------------------------
+// const popup = document.querySelector('.popup')
+// window.addEventListener('click', function (event) {
+
+//     if (event.target.hasAttribute('data-cart')) {
+//         const card = event.target.closest('.menu__item')
+
+//         const productInfo = {
+//             id: card.dataset.id,
+//             imgSrc: card.querySelector('.menu__img').getAttribute('src'),
+//             title: card.querySelector('.menu-item__title').innerText,
+//             price: card.querySelector('.menu__price').innerText,
+//             counter: card.querySelector('[data-counter]').innerText,
+
+//         }
+//         const itemInCart = popup.querySelector(`[data-id="${productInfo.id}"]`);
 
 
+//         if (itemInCart) {
+//             const counterElement = itemInCart.querySelector('.popup__counter');
 
-        }
-        else {
-
-
-            const cartItemHTML = `  
-            <div class="popup__container"  data-id="${productInfo.id}">
-            <div class="popup__img">
-                <img src="${productInfo.imgSrc}" alt="">
-            </div>
-            <div class="popup__title">${productInfo.title}</div>
-            <div class="popup__info">
-                <div class="popup__price">Цена : ${productInfo.price}</div>
-                <div class="popup__box">
-                <div class="popup__title-counter">Кол-во :</div>
-                <div class="popup__counter" data-counter> ${productInfo.counter}</div>
-                </div>
-            </div>
-           </div>`;
+//             counterElement.innerText = parseInt(counterElement.innerText) + parseInt(productInfo.counter);
 
 
 
-            popup.insertAdjacentHTML('beforeend', cartItemHTML)
+//         }
+//         else {
 
 
-        }
-        function showCounter() {
-
-            const cartCounter = document.querySelector('.counter');
-            const cartCounterNumber = document.querySelector('.counter__number');
-            const priceElements = popup.querySelectorAll('.popup__counter');
-            let priceTotal = 0;
-            priceElements.forEach(function (item) {
-                const amountEl = item.closest('.popup__container').querySelector('[data-counter]')
-                priceTotal += parseInt(amountEl.innerText);
-                console.log(amountEl)
-                console.log(priceTotal)
-            })
-            console.log(priceElements)
-            cartCounter.style.display = "block";
-            cartCounterNumber.innerText = parseInt(priceTotal)
+//             const cartItemHTML = `  
+//             <div class="popup__container"  data-id="${productInfo.id}">
+//             <div class="popup__img">
+//                 <img src="${productInfo.imgSrc}" alt="">
+//             </div>
+//             <div class="popup__title">${productInfo.title}</div>
+//             <div class="popup__info">
+//                 <div class="popup__price">Цена : ${productInfo.price}</div>
+//                 <div class="popup__box">
+//                 <div class="popup__title-counter">Кол-во :</div>
+//                 <div class="popup__counter" data-counter> ${productInfo.counter}</div>
+//                 </div>
+//             </div>
+//            </div>`;
 
 
+
+//             popup.insertAdjacentHTML('beforeend', cartItemHTML)
+
+
+//         }
+//         function showCounter() {
+
+//             const cartCounter = document.querySelector('.counter');
+//             const cartCounterNumber = document.querySelector('.counter__number');
+//             const priceElements = popup.querySelectorAll('.popup__counter');
+//             let priceTotal = 0;
+//             priceElements.forEach(function (item) {
+//                 const amountEl = item.closest('.popup__container').querySelector('[data-counter]')
+//                 priceTotal += parseInt(amountEl.innerText);
+//                 console.log(amountEl)
+//                 console.log(priceTotal)
+//             })
+//             console.log(priceElements)
+//             cartCounter.style.display = "block";
+//             cartCounterNumber.innerText = parseInt(priceTotal)
 
 
 
 
 
-        }
-        showCounter()
+
+
+//         }
+//         showCounter()
 
 
 
 
-    }
+//     }
 
 
-})
+// })
 
+import {} from './Footer.js';
+import {} from './Header.js';
+import {} from './Product.js';
 
